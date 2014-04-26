7433d55ebe4fe295fbe73b663ee01497a115ffa9
Matt Hernandez <matt@modulus.io>
2014-04-26 17:17:31 -0400

    Added more tests.
    Adds tests to validate different initialization methods as well as that the
    stream functionality works.

--- spec/sha-stream-spec.js
+++ spec/sha-stream-spec.js
@@ -1,4 +1,7 @@
+/* jshint newcap: false */
+
 var ShaSum = require('../');
+var Stream = require('readable-stream');
 var Transform = require('readable-stream').Transform;
 
 require('chai').should();
@@ -9,5 +12,44 @@ describe('initialization', function () {
 
     sha.should.exist;
     sha.should.be.instanceof(Transform);
+  });
+
+  it('should transform a string when called with new', function (done) {
+    var sha = new ShaSum('sha256');
+    var s = new Stream.Readable();
+    var out = '';
+
+    s._read = function () {};
+    s.push('test');
+    s.push(null);
+
+    s.pipe(sha).on('data', function (chunk) {
+      out += chunk;
+    });
+
+    s.on('end', function () {
+      out.should.equal('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
+
+      done();
+    });
+  });
+
+  it('should transform a string without using new using the default', function (done) {
+    var s = new Stream.Readable();
+    var out = '';
+
+    s._read = function () {};
+    s.push('test');
+    s.push(null);
+
+    s.pipe(ShaSum()).on('data', function (chunk) {
+      out += chunk;
+    });
+
+    s.on('end', function () {
+      out.should.equal('a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
+
+      done();
+    });
   });
 });
